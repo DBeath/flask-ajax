9e21fe65f4f7c21702b9e25832923bc6cbd68e53
David Beath <davidgbeath@gmail.com>
2016-04-26 07:49:53 +1200

    First commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template
+from feedfinder2 import find_feeds
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    msg = 'Hello World 2'
+    return render_template('index.html', msg=msg)
+
+
+@app.route('/get', methods=['POST'])
+def get():
+    print(request)
+    print(request.form)
+    print(request.form.getlist('urlinput'))
+    urls = request.form.getlist('urlinput')
+    print(urls)
+    feeds = []
+    for url in urls:
+        found = find_feeds(url)
+        feeds.extend(found)
+    print(feeds)
+    return jsonify({'result': feeds})
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    feeds = request.form.getlist('feedinput')
+    print(feeds)
+    msg = u'Saved feeds: {0}'.format(feeds)
+    return jsonify({'message': msg})
+
+if __name__ == '__main__':
+    app.run(debug=True)
